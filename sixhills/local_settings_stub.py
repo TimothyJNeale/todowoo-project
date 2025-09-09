d8c8946f432e144f749188046b8af4e9bb59ddec
Six Hills AI <tim.neale@sixhills.ai>
2025-09-09 10:34:08 +0100

    Increased notification messages for settings files

--- sixhills/local_settings_stub.py
+++ sixhills/local_settings_stub.py
@@ -1,2 +1,12 @@
+print("local_settings.py found. Apply local configurations.")
+
 DEBUG = True
 ALLOWED_HOSTS = []
+
+# Add these lines to the bottom of the settings.py file
+
+# try:
+#     from .local_settings import *
+#     print("local_settings.py found. Apply local configurations.")
+# except ImportError:
+#     print("local_settings.py not found. Make sure to create it for local configurations.")
